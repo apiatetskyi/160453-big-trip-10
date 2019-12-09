e4dd7994315efd07823901cdc2ce5ede5466e57f
a.piatetskyi <a.piatetskyi@lucky-labs.com>
2019-12-09 16:45:21 +0200

    Handle escape button press to close event form

--- src/main.js
+++ src/main.js
@@ -12,49 +12,83 @@ import EventForm from './components/event-form';
 import {getEvents} from './mock/event';
 import {getMenu} from './mock/menu';
 import {getFilter} from './mock/filter';
-import {RenderPosition} from './mock/consts';
+import {RenderPosition, KeyboardEnum} from './mock/consts';
 
 const EVENTS_AMOUNT = 10;
+
+/**
+ * Render trip event and event form. Handle toggling between them.
+ *
+ * @param {HTMLElement} parentElement
+ * @param {Object} event
+ */
+const renderEvent = (parentElement, event) => {
+  const eventComponent = new Event(event);
+  const eventFormComponent = new EventForm(event);
+  const editEventButton = eventComponent.getElement().querySelector(`.event__rollup-btn`);
+  const eventFormElement = eventFormComponent.getElement().querySelector(`.event--edit`);
+
+  /**
+   * Escape key press handler.
+   *
+   * @param {KeyboardEvent} evt
+   */
+  const escapeKeyDownHandler = (evt) => {
+    if (evt.key === KeyboardEnum.ESC || evt.key === KeyboardEnum.ESCAPE) {
+      toggleEventForm();
+
+      document.removeEventListener(`keydown`, escapeKeyDownHandler);
+    }
+  };
+
+  /**
+   * Toggle event and event form components.
+   */
+  const toggleEventForm = () => {
+    if (parentElement.contains(eventComponent.getElement())) {
+      parentElement.replaceChild(eventFormComponent.getElement(), eventComponent.getElement());
+    } else {
+      parentElement.replaceChild(eventComponent.getElement(), eventFormComponent.getElement());
+    }
+  };
+
+  editEventButton.addEventListener(`click`, () => {
+    toggleEventForm();
+    document.addEventListener(`keydown`, escapeKeyDownHandler);
+  });
+
+  eventFormElement.addEventListener(`submit`, () => {
+    toggleEventForm();
+  });
+
+  render(parentElement, eventComponent.getElement());
+};
 
 const tripHeaderElement = document.querySelector(`.trip-main`);
 const tripControlsElement = document.querySelector(`.trip-controls`);
 const tripEventsElement = document.querySelector(`.trip-events`);
 const events = getEvents(EVENTS_AMOUNT).sort((current, next) => current.dateStart - next.dateStart);
 
-const tripInfo = new TripInfo(events);
-const menu = new Menu(getMenu());
-const filter = new Filter(getFilter());
-const sorting = new Sorting();
-const board = new Board(events);
+const tripInfoComponent = new TripInfo(events);
+const menuComponent = new Menu(getMenu());
+const filterComponent = new Filter(getFilter());
+const sortingComponent = new Sorting();
+const boardComponent = new Board(events);
 
-render(tripHeaderElement, tripInfo.getElement(), RenderPosition.AFTER_BEGIN);
-render(tripControlsElement.children[0], menu.getElement(), RenderPosition.AFTER_END);
-render(tripControlsElement.children[1], filter.getElement(), RenderPosition.AFTER_END);
-render(tripEventsElement, board.getElement());
-render(tripEventsElement, sorting.getElement());
+render(tripHeaderElement, tripInfoComponent.getElement(), RenderPosition.AFTER_BEGIN);
+render(tripControlsElement.children[0], menuComponent.getElement(), RenderPosition.AFTER_END);
+render(tripControlsElement.children[1], filterComponent.getElement(), RenderPosition.AFTER_END);
+render(tripEventsElement, boardComponent.getElement());
+render(tripEventsElement, sortingComponent.getElement());
 
 [...groupEventsByDays(events)].forEach((day, index) => {
   const [dayTimestamp, dayEvents] = day;
   const tripDay = new TripDay(dayTimestamp, index + 1);
   const eventsElement = tripDay.getElement().querySelector(`.trip-events__list`);
 
-  // TODO: remove `data` from variable name
-  dayEvents.forEach((eventData) => {
-    const event = new Event(eventData);
-    const eventForm = new EventForm(eventData);
-    const editEventButton = event.getElement().querySelector(`.event__rollup-btn`);
-    const eventFormElement = eventForm.getElement().querySelector(`.event--edit`);
-
-    editEventButton.addEventListener(`click`, () => {
-      eventsElement.replaceChild(eventForm.getElement(), event.getElement());
-    });
-
-    eventFormElement.addEventListener(`submit`, () => {
-      eventsElement.replaceChild(event.getElement(), eventForm.getElement());
-    });
-
-    render(eventsElement, event.getElement());
+  dayEvents.forEach((event) => {
+    renderEvent(eventsElement, event);
   });
 
-  render(board.getElement(), tripDay.getElement());
+  render(boardComponent.getElement(), tripDay.getElement());
 });
